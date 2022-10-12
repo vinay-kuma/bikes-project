7733016df430f3f3ed7234c12e53e57c34cf9dc7
vinay-kuma <Vinayvins9948@gmail.com>
2022-10-12 10:43:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { UsersComponent } from './users/users.component';
+import { BikeComponent } from './bikes/bike/bike.component';
+import { UserComponent } from './users/user/user.component';
+
+import { EditBikeComponent } from './bikes/edit-bike/edit-bike.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BikesService } from './bikes/bikes.service';
+import { BikeResolver } from './bikes/bike/bike-resolver.service';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BikesComponent,
+    UsersComponent,
+    BikeComponent,
+    UserComponent,
+    EditBikeComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+
   ],
-  providers: [],
+  providers: [BikesService, AuthService, AuthGuard, BikeResolver ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
